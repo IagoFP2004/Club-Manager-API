f5e85faeb5736e55e397b7b119fc223b42f26e0f
IagoFP2004 <iagofranciscoperez123@gmail.com>
2025-10-06 18:00:52 +0200

    Busqueda por email con token necesario implementado

--- src/Controller/UsuarioController.php
+++ src/Controller/UsuarioController.php
@@ -1,24 +1,28 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
-
-
+use Symfony\Component\HttpFoundation\Response;
 class UsuarioController extends AbstractController
 {
-    #[Route('/api/me', name: 'api_me', methods: ['GET'])]
-    public function __getUser():JsonResponse
+    #[Route('/api/{email}', name: 'api_email', methods: ['GET'])]
+    public function getByEmail(EntityManagerInterface $entityManager, $email): Response
     {
-        $user = $this->getUser();
-        return new JsonResponse([
-            'id' => $user->getId(),
-            'nombre' => $user->getNombre(),
-            'email' => $user->getEmail(),
-        ]);
+        $user = $entityManager->getRepository(User::class)->findOneBy(['email' => $email]);
 
+        if (!$user) {
+            return $this->json(['message' => 'User not found'], 404);
+        }
+
+        $data = [
+            'id' => $user->getId(),
+            'nombres' => $user->getNombre(),
+            'email' => $user->getEmail(),
+        ];
+        return $this->json($data);
     }
 }
